052d6971f52b82f5b27ab9b8fd33d0d829520a1c
darah98 <darashalabi@gmail.com>
2020-04-06 22:02:29 +0300

    lab07done

--- README.md
+++ README.md
@@ -22,32 +22,42 @@ We are using JavaScrip language to script this project. We are also applying Nod
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 -->
 
-Number and name of feature: 1 REPO SET-UP
+## Number and name of feature: 1 Data formatting
+
+Estimate of time needed to complete: 30 MINUTES
+
+Start time: 14:00
+
+Finish time: 14:30
+
+Actual time needed to complete: 30 MINUTES
+
+## Number and name of feature: 2 /LOCATIONS
 
 Estimate of time needed to complete: 1 HOUR
 
 Start time: 15:00
 
-Finish time: 17:00
+Finish time: 16:00
 
-Actual time needed to complete: 2 HOURS
+Actual time needed to complete: 1 HOUR
 
-Number and name of feature: 2 /LOCATION
-
-Estimate of time needed to complete: 2 HOURS
-
-Start time: 17:30
-
-Finish time: 18:30
-
-Actual time needed to complete: 1 HOOUR
-
-Number and name of feature: 3 /WEATHER
+## Number and name of feature: 3 /WEATHER
 
 Estimate of time needed to complete: 2 HOURS
 
-Start time: 18:30
+Start time: 16:00
 
-Finish time: 20:00
+Finish time: 18:00
 
 Actual time needed to complete: 2 HOURS
+
+## Number and name of feature: 3 /TRAILS
+
+Estimate of time needed to complete: 2 HOURS
+
+Start time: 18:00
+
+Finish time: 21:00
+
+Actual time needed to complete: 3 HOURS
--- server.js
+++ server.js
@@ -12,34 +12,100 @@ server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 server.get('/', (req, res) => {
-  res.status(200).send('HEERREEE');
+  res.send('HEERREEE');
 });
-server.get('/location', (req, res) => {
-  const geoData = require('./data/geo.json');
+server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+server.get('/trails', trailHandler);
+
+function locationHandler(req, res) {
   const city = req.query.city;
-  const locationData = new Location(city, geoData);
-  res.send(locationData);
-});
+  getLocDataFromAPI(city).then((locationData) =>
+    res.status(200).json(locationData)
+  );
+}
+function getLocDataFromAPI(city) {
+  let key = process.env.GEOCODE_API_KEY;
+  const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+  return superagent.get(url).then((geoData) => {
+    const locationData = new Location(city, geoData.body);
+    return locationData;
+  });
+}
 function Location(city, geoData) {
-  this.search_query = city;
+  this.city = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
-
-server.get('/weather', (req, res) => {
-  const weatherData = require('./data/weather.json');
+function weatherHandler(req, res) {
   const city = req.query.city;
-//   let weatherArr = [];
-  let weatherArr = weatherData.data.map((value) => {
-     new Weather(value.weather.description, value.datetime);
-    // weatherArr.push(forecast);
+  getWeatherDataFromAPI(city).then((weatherData) =>
+    res.status(200).json(weatherData)
+  );
+}
+function getWeatherDataFromAPI(city) {
+  let key = process.env.WEATHER_API_KEY;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+  return superagent.get(url).then((weatherData) => {
+    let weatherArr = weatherData.body.data.map((value) => {
+      return new Weather(value.weather.description, value.datetime);
+    });
+    return weatherArr;
   });
-  res.send(weatherArr);
-});
+}
 function Weather(description, datetime) {
-  this.description = description;
+  this.forecast = description;
   this.time = datetime;
+}
+function trailHandler(req, res) {
+  const lat = req.query.latitude;
+  const lon = req.query.longitude;
+  getTrailDataFromAPI(lat, lon).then((trailData) =>
+    res.status(200).json(trailData)
+  );
+}
+function getTrailDataFromAPI(lat, lon) {
+  let key = process.env.TRAIL_API_KEY;
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&key=${key}`;
+  return superagent.get(url).then((trailData) => {
+    let trailArr = trailData.body.trails.map((value) => {
+      return new Trail(
+        value.name,
+        value.location,
+        value.length,
+        value.stars,
+        value.starVotes,
+        value.summary,
+        value.url,
+        value.conditionDetails,
+        value.conditionDate
+      );
+    });
+    return trailArr;
+  });
+}
+function Trail(
+  name,
+  location,
+  length,
+  stars,
+  starVotes,
+  summary,
+  url,
+  conditionDetails,
+  conditionDate
+) {
+  this.name = name;
+  this.location = location;
+  this.length = length;
+  this.stars = stars;
+  this.star_votes = starVotes;
+  this.summary = summary;
+  this.trail_url = url;
+  this.conditions = conditionDetails;
+  this.condition_date = conditionDate.slice(0, -9);
+  this.condition_time = conditionDate.slice(-9);
 }
 
 server.use('*', (req, res) => {
