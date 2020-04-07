3cfbcba97983f45a35219ac119c1c26341c3d8b9
darah98 <darashalabi@gmail.com>
2020-04-07 23:07:49 +0300

    lab08 done

--- server.js
+++ server.js
@@ -3,45 +3,59 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg= require('pg');
 
+const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3000;
 const server = express();
 server.use(cors());
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
 server.get('/', (req, res) => {
   res.send('HEERREEE');
 });
 server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
 server.get('/trails', trailHandler);
-
 function locationHandler(req, res) {
   const city = req.query.city;
-  getLocDataFromAPI(city).then((locationData) =>
-    res.status(200).json(locationData)
-  );
+  let SQL= 'SELECT * FROM cities WHERE city_name LIKE $1';
+    client.query(SQL, [city]).then((results)=>{
+      if (results.rows.length){
+        res.status(200).json(results.rows[0]);
+      } else { 
+        getLocDataFromAPI(city).then((locationData) =>
+        res.status(200).json(locationData)
+        );
+      }    
+    })
 }
 function getLocDataFromAPI(city) {
   let key = process.env.GEOCODE_API_KEY;
   const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-  return superagent.get(url).then((geoData) => {
+  return superagent.get(url)
+  .then((geoData) => {
     const locationData = new Location(city, geoData.body);
+    let sql = 'INSERT INTO cities (city_name, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
+    let safeValues=[
+    locationData.city_name,
+    locationData.formatted_query,
+    locationData.latitude,
+    locationData.longitude
+    ];
+    client.query(sql, safeValues);
     return locationData;
-  });
+    });
 }
 function Location(city, geoData) {
-  this.city = city;
+  this.city_name = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
 function weatherHandler(req, res) {
-  const city = req.query.city;
+  const city = req.query.city_name;
   getWeatherDataFromAPI(city).then((weatherData) =>
-    res.status(200).json(weatherData)
+  res.status(200).json(weatherData)
   );
 }
 function getWeatherDataFromAPI(city) {
@@ -62,12 +76,12 @@ function trailHandler(req, res) {
   const lat = req.query.latitude;
   const lon = req.query.longitude;
   getTrailDataFromAPI(lat, lon).then((trailData) =>
-    res.status(200).json(trailData)
+  res.status(200).json(trailData)
   );
 }
 function getTrailDataFromAPI(lat, lon) {
   let key = process.env.TRAIL_API_KEY;
-  const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&key=${key}`;
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${key}`;
   return superagent.get(url).then((trailData) => {
     let trailArr = trailData.body.trails.map((value) => {
       return new Trail(
@@ -80,7 +94,7 @@ function getTrailDataFromAPI(lat, lon) {
         value.url,
         value.conditionDetails,
         value.conditionDate
-      );
+        );
     });
     return trailArr;
   });
@@ -95,23 +109,28 @@ function Trail(
   url,
   conditionDetails,
   conditionDate
-) {
-  this.name = name;
-  this.location = location;
-  this.length = length;
-  this.stars = stars;
-  this.star_votes = starVotes;
-  this.summary = summary;
-  this.trail_url = url;
-  this.conditions = conditionDetails;
-  this.condition_date = conditionDate.slice(0, -9);
-  this.condition_time = conditionDate.slice(-9);
+  ) {
+    this.name = name;
+    this.location = location;
+    this.length = length;
+    this.stars = stars;
+    this.star_votes = starVotes;
+    this.summary = summary;
+    this.trail_url = url;
+    this.conditions = conditionDetails;
+    this.condition_date = conditionDate.slice(0, -9);
+    this.condition_time = conditionDate.slice(-9);
 }
 
 server.use('*', (req, res) => {
   res.status(500).send('Sorry, something went wrong!');
 });
-
 server.use((error, req, res) => {
   res.status(500).send(error);
+});
+client.connect()
+.then(()=>{
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 });
